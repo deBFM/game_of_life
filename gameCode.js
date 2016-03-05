a65d479534667386c58a5561973fe74df464e656
deBFM <hubidennis@web.de>
2016-03-05 02:10:10 +0100

    code++

--- gameCode.js
+++ gameCode.js
@@ -1,4 +1,6 @@
 /**
+ * Game of Life
+ *
  * @author Dennis Kronbügel
  */
 
@@ -11,30 +13,52 @@ var gameData = {
 };
 
 var gameCode = {
+	"renderer" : null, /* CanvasRenderingContext2D */
 
 	"init" : function() {
-		console.log("init");
+		this.renderer = document.getElementById("gameview").getContext("2d");
 		this.initGrid();
 	},
 
 	"initGrid" : function() {
-		console.log("initGrid");
-		gameData.grid = [["a1", "a2"], ["b1", "b2"]];
+		gameData.grid = new Array(GRID_X_COUNT);
+		for (var i = 0; i <= GRID_X_COUNT; i++) {
+			gameData.grid[i] = new Array(GRID_Y_COUNT);
+		}
+	},
 
+	"drawGrid" : function() {
+		for (var x = 0; x <= 50; x = x + 1) {
+			for (var y = 0; y <= 50; y = y + 1) {
+				if (gameData.grid[x][y] === true) {
+					this.renderer.fillRect(x * GRID_FIELD_SIZE, y * GRID_FIELD_SIZE, GRID_FIELD_SIZE, GRID_FIELD_SIZE);
+				} else {
+					this.renderer.rect(x * GRID_FIELD_SIZE, y * GRID_FIELD_SIZE, GRID_FIELD_SIZE, GRID_FIELD_SIZE);
+				}
+			}
+		}
+		this.renderer.strokeStyle = "rgb(240,240,240)";
+		this.renderer.stroke();
 	},
 
 	"nextIteration" : function() {
-		var canvas = document.getElementById("gameview");
-		var renderer = canvas.getContext("2d");
-
-		for (var x = 0; x <= 50; x = x + 1) {
-			for (var y = 0; y <= 50; y = y + 1) {
-				renderer.rect(x * GRID_FIELD_SIZE, y * GRID_FIELD_SIZE, GRID_FIELD_SIZE, GRID_FIELD_SIZE);
-			}
-		}
-		renderer.stroke();
+		//1. berechnen auf grundlage von gameDate.grid -> Ergebnisse gehen aber in ein pufferGrid.
+		//2. puffergrid auf das gameDate.grid bügeln.
+		
+		
+		this.drawGrid();
 	}
 };
 
-//----main----
-gameCode.init();
+//-----------------------------
+document.addEventListener("DOMContentLoaded", function() {
+	gameCode.init();
+	//Testdaten
+	gameData.grid[1][2] = true;
+	gameData.grid[2][2] = true;
+	gameData.grid[2][3] = true;
+	gameData.grid[3][4] = true;
+	gameData.grid[3][1] = true;
+
+});
+
