c9d7b4d40ca19257d8011b5967d0ba4e0280c709
deBFM <hubidennis@web.de>
2016-03-06 14:43:20 +0100

    Alpha 1

--- gameCode.js
+++ gameCode.js
@@ -27,6 +27,7 @@ var gameData = {
 
 var gameCode = {
 	"onCycle" : false,
+	"delay" : 30,
 	"intervalID" : null,
 	"renderer" : null, /* CanvasRenderingContext2D */
 
@@ -46,12 +47,19 @@ var gameCode = {
 		}
 	},
 
+	"intervalDelay" : function(delay) {
+		this.delay = delay;
+		if (this.intervalID != null) {
+			this.pauseGame();
+			this.startGame();
+		} 
+	},
+
 	"startGame" : function() {
 		if (this.intervalID === null) {
 			this.intervalID = window.setInterval(function() {
-				console.log("x");
 				gameCode.nextIteration();
-			}, 50);
+			}, this.delay);
 		}
 	},
 
@@ -134,7 +142,7 @@ var gameCode = {
 				}
 			}
 		}
-		// update grid wiht next iteration
+		// update grid with next iteration
 		for (var x = 0; x < GRID_X_COUNT; x++) {
 			for (var y = 0; y < GRID_Y_COUNT; y++) {
 				gameData.grid[x][y].alive = gameData.grid[x][y].nextIterationAlive;
@@ -167,30 +175,96 @@ var gameCode = {
 	},
 
 	"populateWithTestData1" : function() {
+		gameData.grid[45][50] = new Cell(true);
+		gameData.grid[46][50] = new Cell(true);
+		gameData.grid[47][50] = new Cell(true);
+		gameData.grid[47][51] = new Cell(true);
+		gameData.grid[48][57] = new Cell(true);
+		gameData.grid[49][50] = new Cell(true);
+		gameData.grid[49][52] = new Cell(true);
+		gameData.grid[50][48] = new Cell(true);		
 		gameData.grid[50][50] = new Cell(true);
 		gameData.grid[50][51] = new Cell(true);
 		gameData.grid[50][52] = new Cell(true);
 		gameData.grid[50][53] = new Cell(true);
 		gameData.grid[51][52] = new Cell(true);
+		gameData.grid[52][51] = new Cell(true);
 		gameData.grid[52][52] = new Cell(true);
 		gameData.grid[54][55] = new Cell(true);
-		gameData.grid[49][50] = new Cell(true);
-		gameData.grid[48][57] = new Cell(true);
-		gameData.grid[47][50] = new Cell(true);
-		gameData.grid[50][48] = new Cell(true);		
-		gameData.grid[45][50] = new Cell(true);
-		gameData.grid[47][50] = new Cell(true);
-		gameData.grid[46][50] = new Cell(true);
-		gameData.grid[49][52] = new Cell(true);
 		gameData.grid[55][52] = new Cell(true);
 		gameData.grid[57][52] = new Cell(true);
-		gameData.grid[52][52] = new Cell(true);
 		gameData.grid[60][60] = new Cell(true);
 		gameData.grid[60][61] = new Cell(true);
 		gameData.grid[60][62] = new Cell(true);
 		gameData.grid[60][65] = new Cell(true);
 		this.drawGrid();
 	},
+	
+	"populateWithTestData2" : function() {
+		gameData.grid[70][51] = new Cell(true);		
+		gameData.grid[70][49] = new Cell(true);
+		gameData.grid[70][48] = new Cell(true);
+		gameData.grid[70][49] = new Cell(true);
+		gameData.grid[70][51] = new Cell(true);
+		gameData.grid[70][52] = new Cell(true);
+		gameData.grid[70][55] = new Cell(true);
+		gameData.grid[75][49] = new Cell(true);
+		gameData.grid[71][50] = new Cell(true);
+		gameData.grid[72][50] = new Cell(true);
+		gameData.grid[72][52] = new Cell(true);
+		gameData.grid[73][50] = new Cell(true);
+		gameData.grid[74][50] = new Cell(true);
+		gameData.grid[75][47] = new Cell(true);
+		gameData.grid[75][46] = new Cell(true);
+		gameData.grid[75][48] = new Cell(true);
+		gameData.grid[75][49] = new Cell(true);
+		gameData.grid[75][50] = new Cell(true);
+		gameData.grid[75][51] = new Cell(true);
+		gameData.grid[75][50] = new Cell(true);
+		gameData.grid[76][50] = new Cell(true);
+		gameData.grid[77][50] = new Cell(true);
+		gameData.grid[77][52] = new Cell(true);
+		gameData.grid[78][50] = new Cell(true);
+		gameData.grid[79][50] = new Cell(true);
+		this.drawGrid();
+	},
+	
+	"populateWithTestData3" : function() {
+		gameData.grid[140][51] = new Cell(true);
+		gameData.grid[140][52] = new Cell(true);
+		gameData.grid[140][55] = new Cell(true);
+		gameData.grid[145][49] = new Cell(true);
+		gameData.grid[145][48] = new Cell(true);
+		gameData.grid[145][47] = new Cell(true);
+		gameData.grid[150][52] = new Cell(true);
+		gameData.grid[150][42] = new Cell(true);
+		gameData.grid[150][50] = new Cell(true);
+		gameData.grid[150][51] = new Cell(true);
+		gameData.grid[150][54] = new Cell(true);		
+		gameData.grid[150][55] = new Cell(true);
+		gameData.grid[150][56] = new Cell(true);
+		gameData.grid[151][51] = new Cell(true);
+		gameData.grid[152][52] = new Cell(true);
+		gameData.grid[152][50] = new Cell(true);
+		gameData.grid[153][53] = new Cell(true);
+		gameData.grid[154][54] = new Cell(true);
+		gameData.grid[155][41] = new Cell(true);
+		gameData.grid[155][44] = new Cell(true);
+		gameData.grid[155][45] = new Cell(true);
+		gameData.grid[155][47] = new Cell(true);
+		gameData.grid[155][48] = new Cell(true);
+		gameData.grid[155][49] = new Cell(true);
+		gameData.grid[155][53] = new Cell(true);
+		gameData.grid[155][55] = new Cell(true);
+		gameData.grid[156][51] = new Cell(true);
+		gameData.grid[157][52] = new Cell(true);
+		gameData.grid[157][53] = new Cell(true);
+		gameData.grid[158][53] = new Cell(true);
+		gameData.grid[159][54] = new Cell(true);
+
+		this.drawGrid();
+	},	
+	
 
 	"onCanvasClick" : function(event) {
 		var mod = Math.round(GRID_FIELD_SIZE / 2);
